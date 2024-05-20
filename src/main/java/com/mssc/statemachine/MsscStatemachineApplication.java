78e1cbcc0d94948e997ce24e869d6ea518bf3c33
majdhaidar <haidarmajd@hotmail.com>
2024-05-20 22:22:51 +0300

    initial

---
+++ src/main/java/com/mssc/statemachine/MsscStatemachineApplication.java
@@ -0,0 +1,13 @@
+package com.mssc.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscStatemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscStatemachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mssc/statemachine/MsscStatemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mssc.statemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscStatemachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
